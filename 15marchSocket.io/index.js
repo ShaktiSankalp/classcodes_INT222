6da6e348677c31b9fd355cf67f0288d20a04fa9d
SonyT0pran0 <119934629+ShaktiSankalp@users.noreply.github.com>
2024-03-27 15:16:19 +0530

    .

--- 15marchSocket.io/index.js
+++ 15marchSocket.io/index.js
@@ -14,8 +14,27 @@ app.use(express.static(path.join(__dirname))) //must understand this line plaaz 
 
 const io = socketIO(server); //server side instance of socketIO
 
+const users=[];
+
 io.on("connection",(socket)=>{
     console.log('A user is connected to the server');
+
+    socket.on("setUsername",(username)=>{
+        users[socket.id]= username;
+        socket.broadcast.emit("userJoined",`<h3>${username} has joined the chat.</h3>`);
+    })
+    socket.on("chat message",(msg)=>{
+        const username = users[socket.id] || "Anonymous";
+        io.emit("chat message",`${username}: ${msg}`);
+    })
+
+    socket.on("disconnect",()=>{
+        const username = users[socket.id] || "Anonymous"; 
+        delete users[socket.id];
+        socket.broadcast.emit("userLeft",`<h3>${username} has left the chat</h3>`);
+
+    })
+
 })
 
 
